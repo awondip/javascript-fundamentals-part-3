c16567a12d5b06168af9e5613eda16a0ce35275c
dylane <awondipdylane047@.com>
2022-03-18 11:27:18 +0100

    The syntax error

---
+++ syntax-error.js
@@ -0,0 +1,7 @@
+// Syntax Error
+// A syntax error occurs when the code you are trying to run is not written correctly, i.e., in accordance with the grammatical rules of JavaScript. For example this:
+
+function helloWorld() {
+   console.log "Hello World!"
+}
+// will throw the following error, because we forgot the parentheses for console.log()!
