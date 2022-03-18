0d5bfc7bb55006b0fd7825d68da3916301d3adba
dylane <awondipdylane047@.com>
2022-03-18 11:13:11 +0100

    Expalaning the Reffrence error

---
+++ Refference-Error.js
@@ -0,0 +1,9 @@
+// A ReferenceError is thrown when one refers to a variable that is not declared and/or initialized within the current scope.
+// Different errors of this type have different messages based on what is causing the ReferenceError. 
+
+//Example
+
+const a = "Hello"
+const b = "World"
+
+console.log(c)
